f2f1643407a7758bb3451c96d2b8439004f89aa0
Van <van_daniel_naldoza@dlsl.edu.ph>
2025-04-11 00:32:59 +0800

    m

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import AddEntryScreen from './screens/AddEntryScreen';
+import HomeScreen from '../screens/HomeScreen';
+import AddEntryScreen from '../screens/AddEntryScreen';
 
 const Stack = createNativeStackNavigator();
 
